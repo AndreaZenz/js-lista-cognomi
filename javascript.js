fdc2553912860f21cba6d8af373cdbdc18d1c14d
AndreaZenz <zenzero.ere@gmail.com>
2021-04-13 16:44:32 +0200

    all lowercase

--- javascript.js
+++ javascript.js
@@ -7,18 +7,29 @@ Consegna:
 
 
 // 1. chiedi all’utente il cognome
-var cognome = prompt("Qual'è il tuo cognome?")
-var cognomiDisponibili = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
-
+var cognome = prompt("Qual'è il tuo cognome?");
+var cognomiDisponibili = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-cognomiDisponibili.push(cognome)
+cognomiDisponibili.push(cognome);
+
+
 
 // 3. stampa la lista ordinata alfabeticamente
-cognomiDisponibili.sort()
+cognomiDisponibili.sort();
 
-document.getElementById("surname").innerHTML = "<li>" + cognomiDisponibili + "</li>"
+var sorted = [];
+for (var i = 0; i < cognomiDisponibili.length; i++) {
+    sorted.push(cognomiDisponibili[i].toLowerCase());
+}
 
-// console.log(cognomiDisponibili)
+sorted.sort();
 
-cognomiDisponibili.indexOf
+console.log(sorted);
+
+
+
+document.getElementById("surname").innerHTML = "<li>" + sorted + "</li>"
+
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+document.getElementById("order").innerHTML = "L'ordine del tuo cognome è " + (sorted.indexOf(cognome) + 1);
